557c1021ddb281bc9bad1552c59b4ff632122514
spidyz <spidy3477@gmail.com>
2024-10-22 22:29:06 +0530

    Money transfer project using temporal

---
+++ src/main/java/moneytransfer/TransactionDetails.java
@@ -0,0 +1,12 @@
+package moneytransfer;// @@@SNIPSTART money-transfer-java-transaction-details
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize(as = CoreTransactionDetails.class)
+public interface TransactionDetails {
+    String getSourceAccountId();
+    String getDestinationAccountId();
+    String getTransactionReferenceId();
+    int getAmountToTransfer();
+}
+// @@@SNIPEND
+
